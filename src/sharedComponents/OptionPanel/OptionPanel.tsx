8dfa966c8a00f2ef79389b4fb66e59422a15b1c6
flavio912 <flavio.arcaro912@gmail.com>
2020-07-08 20:57:23 +0200

    Fix burger menu radio option

--- src/sharedComponents/OptionPanel/OptionPanel.tsx
+++ src/sharedComponents/OptionPanel/OptionPanel.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import styled from 'styled-components';
 import ArrowSVG from '../../assets/images/arrow.svg';
 import CustomRadio from '../CustomRadio';
@@ -22,33 +22,18 @@ const OptionModal = ({isShow, hidePanel, customStyle}: Props) => {
       <Title>Options</Title>
       <TourP><img src={ArrowSVG} style={{marginRight:'10px', width: '40px'}} />Tour control: <span style={{fontWeight: 700, marginLeft: '0.5rem'}}>them</span></TourP>
       <CustomUl>
-        <li className="d-flex align-items-center">
-          <CustomRadio 
-            name="chat-on" 
-            value="chat on" 
-            label="Chat on" 
-            checked={selectedValue === 'chat on'} 
-            onChange={(value:string) => {setValue(value)}}
-          />
-        </li>
-        <li className="d-flex align-items-center">
-          <CustomRadio 
-            name="audio-off" 
-            value="audio off" 
-            label="Audio off" 
-            checked={selectedValue === 'audio off'} 
-            onChange={(value:string) => {setValue(value)}}
-          />
-        </li>
-        <li className="d-flex align-items-center">
-          <CustomRadio 
-            name="save-transcript" 
-            value="save transcript" 
-            label="Save transcript" 
-            checked={selectedValue === 'save transcript'} 
-            onChange={(value:string) => {setValue(value)}}
-          />
-        </li>
+        <label className="custom-checkbox">Chat on
+          <input type="checkbox" />
+          <span className="checkmark"></span>
+        </label>
+        <label className="custom-checkbox">Audio off
+          <input type="checkbox" />
+          <span className="checkmark"></span>
+        </label>
+        <label className="custom-checkbox">Save transcript
+          <input type="checkbox" />
+          <span className="checkmark"></span>
+        </label>        
       </CustomUl>      
 
       <SubmitButton variant="outline-primary" onClick={() => {hidePanel(true)}}>Submit</SubmitButton>
@@ -78,6 +63,73 @@ const CustomUl = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
+
+  .custom-checkbox {
+    display: block;
+    position: relative;
+    padding-left: 35px;
+    margin-bottom: 12px;
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  /* Hide the browser's default checkbox */
+  .custom-checkbox input {
+    position: absolute;
+    opacity: 0;
+    cursor: pointer;
+    height: 0;
+    width: 0;
+  }
+
+  /* Create a custom checkbox */
+  .checkmark {
+    position: absolute;
+    top: 0;
+    left: 0;
+    height: 25px;
+    width: 25px;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+
+  /* On mouse-over, add a grey background color */
+  .custom-checkbox:hover input ~ .checkmark {
+    background-color: #ccc;
+  }
+
+  /* When the checkbox is checked, add a blue background */
+  .custom-checkbox input:checked ~ .checkmark {
+    background-color: #00AFD3;
+  }
+
+  /* Create the checkmark/indicator (hidden when not checked) */
+  .checkmark:after {
+    content: "";
+    position: absolute;
+    display: none;
+  }
+
+  /* Show the checkmark when checked */
+  .custom-checkbox input:checked ~ .checkmark:after {
+    display: block;
+  }
+
+  /* Style the checkmark/indicator */
+  .custom-checkbox .checkmark:after {
+    left: 9px;
+    top: 5px;
+    width: 7px;
+    height: 12px;
+    border: solid white;
+    border-width: 0 3px 3px 0;
+    -webkit-transform: rotate(45deg);
+    -ms-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
 `
 
 const SubmitButton = styled(Button)`
